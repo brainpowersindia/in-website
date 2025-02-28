024a24a990e8d30bd672db6136c8d131d0fb6f96
brainpowersindia <133396553+brainpowersindia@users.noreply.github.com>
2025-02-28 16:23:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ function callUs() {
 
 function whatsappUs() {
 
+        <!-- Event snippet for Whatsapp Button Clicked conversion page
+    In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+    <script>
+    function gtag_report_conversion(url) {
+      var callback = function () {
+        if (typeof(url) != 'undefined') {
+          window.location = url;
+        }
+      };
+      gtag('event', 'conversion', {
+          'send_to': 'AW-16461513419/ch44CIeqi6QaEMuFu6k9',
+          'value': 1.0,
+          'currency': 'INR',
+          'event_callback': callback
+      });
+      return false;
+    }
+    </script>
+
+
     window.open("https://wa.me/918433569122/?text=मला%20अधिक%20माहिती%20हवी%20आहे.","_blank");
 }
 // Function to be called when the section is visible
